504b35c4c005e3211ef7814536ca5442f1252c90
yorgos <stratis@ece.neu.edu>
2024-12-07 14:41:42 -0500

    Created Automaton class.

--- src/cellular_automaton.py
+++ src/cellular_automaton.py
@@ -1,26 +1,64 @@
 import numpy as np
 
 
+class Automaton:
+    """The cellular automaton class."""
+    def __init__(self, neigh, rule_fn, rule_args, geometry):
+        self.neigh = neigh
+        self.digits = 2**(2 * neigh + 1) - 1
+        self.rule_fn = rule_fn
+        self.rule_args = rule_args
+        self.geometry = geometry
+        self.lattice = np.zeros(self.geometry)
+
+    def initialize(self, initial_cond):
+        """
+        Set value to one at all the indices in the array intial_cond.
+        """ 
+        self.lattice[initial_cond] = 1 
+
+    def get_configuration(self):
+        """
+        Get current lattice configuration.
+        """
+        self.current_config = self.lattice.copy()
+
+        return self.current_config
+
+    def update(self):
+        """
+        Update lattice configuration given the rule and current configuration.
+        """
+        past_config = self.get_configuration()
+        for i_site in range(self.geometry):
+            neighbor_vals = np.array(
+             [past_config[(i_site + k) % self.geometry]
+              for k in range(-self.neigh, self.neigh + 1)]
+            )
+            self.lattice[i_site] = self.rule_fn(neighbor_vals,
+                                                **self.rule_args)
+
+
 def get_rule(rule_num, digits):
     """Define update rule according to its numerical representation."""
-    rule = []
+    l_rule = []
     for i in range(digits, -1, -1):
-        rule.append(rule_num // 2**i)
+        l_rule.append(rule_num // 2**i)
         rule_num %= (2**i)
 
-    return rule
+    return l_rule
 
 
-def cell_rule(neighbor_vals, rule, digits, neigh):
+def rule_vanilla(neighbor_vals, l_rule, digits, neigh):
     """
     The cellular automato governing rule.
     """
-    # Convert the binary string of the neighbring data into a decimal
+    # Convert the binary string of the neighboring data into a decimal
     # number. This decimal number corresponds to index of the binary
     # representation of the rule.
     exp = np.array([2**i for i in range(2 * neigh, -1, -1)])
     ind = int(np.sum(exp * neighbor_vals))
-    a = rule[digits - ind]
+    a = l_rule[digits - ind]
     
     return a
 
--- src/example_1d.py
+++ src/example_1d.py
@@ -1,37 +1,37 @@
 import numpy as np
 from visualization import PlotTool
-from cellular_automaton import get_rule, cell_rule
+from cellular_automaton import get_rule, rule_vanilla, Automaton
 
 
 # Define the system's  geometry.
 # Number of sites.
 N_SITES = 100
 # Initial condition. We provide the indices of the sites with value 1.
-IC = [50]
+INITIAL_COND = [50]
 # Nearest neighbors used to update current site.
 NEIGH = 1
 digits = 2**(2 * NEIGH + 1) - 1
 T_MAX = 500
 RULE_NUM = 110
-rule = get_rule(RULE_NUM, digits)
+l_rule = get_rule(RULE_NUM, digits)
+rule_fn = rule_vanilla
+rule_args = {"l_rule": l_rule, "digits": digits, "neigh": NEIGH}
 XLABEL = "Time"
 YLABEL = "Position"
 DPI = 600
 PICS_DIR = "../pics"
 PLOT_TYPE = "image"
 LABEL = None
-FIGNAME = "cellular_automato_1d"
+FIGNAME = "cellular_automaton_1d"
 l_times = np.arange(T_MAX)
+automaton = Automaton(NEIGH, rule_fn, rule_args, N_SITES)
+automaton.initialize(INITIAL_COND)
 data = np.zeros((T_MAX, N_SITES))
-# Fill with ones at the locations indicated by the array IC.
-for i_occupied in IC:
-    data[0, i_occupied] = 1 
+data[0] = automaton.lattice.copy()
 plot_evolution = PlotTool(XLABEL, YLABEL, PICS_DIR, DPI)
 for time_ in l_times[1:]:
-    for i_site in range(N_SITES):
-        neighbor_vals = np.array([data[time_ - 1, (i_site + k) % N_SITES]
-                                  for k in range(-NEIGH, NEIGH + 1)])
-        data[time_, i_site] = cell_rule(neighbor_vals, rule, digits, NEIGH)
+    automaton.update()
+    data[time_] = automaton.get_configuration()
 data = np.transpose(np.array(data))
 plot_evolution.plot_figure(data, PLOT_TYPE, LABEL, FIGNAME)
 
